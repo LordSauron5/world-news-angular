c53717ed3bc064c2712ab8bbba8fb79a4230fe48
Tyron Aricun <tyronaricum1@gmail.com>
2023-03-19 23:20:19 +0200

    fix paginator and endless error notification

--- src/app/news-api/na-article-list/na-article-list.component.ts
+++ src/app/news-api/na-article-list/na-article-list.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { NewsApiService, Article } from '../news-api.service';
-
+import { Article, NewsApiService } from '../news-api.service';
+ 
 @Component({
   selector: 'app-na-article-list',
   templateUrl: './na-article-list.component.html',
-  styleUrls: ['./na-article-list.component.css']
+  styleUrls: ['./na-article-list.component.css'],
 })
 export class NaArticleListComponent implements OnInit {
-
-  articles: Article[]
-
-  constructor(private newsApiService: NewsApiService) { 
+  // Declarations
+  articles?: Article[];
+ 
+  // Initializations
+  numberOfPages = 0;
+ 
+  constructor(private newsApiService: NewsApiService) {
     this.newsApiService.pagesOutput.subscribe((articles) => {
-      this.articles = articles
+      this.articles = articles;
     });
-
+    this.newsApiService.numberOfPages.subscribe((numberOfPages) => {
+      this.numberOfPages = numberOfPages;
+    });
+ 
     this.newsApiService.getPage(1);
   }
-
-  ngOnInit(): void {
+ 
+  ngOnInit(): void {}
+ 
+  onPageChanged(page: number) {
+    this.newsApiService.getPage(page);
   }
-
 }
--- src/app/shared/paginator/paginator.component.ts
+++ src/app/shared/paginator/paginator.component.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+     
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
-  styleUrls: ['./paginator.component.css']
+  styleUrls: ['./paginator.component.css'],
 })
 export class PaginatorComponent implements OnInit {
-  @Input() numberOfPages: number;
-  pageOptions: number[];
-  
+  @Input('numberOfPages') set _numberOfPages(numberOfPages: number) {
+    this.numberOfPages = numberOfPages;
+    this.setPageOptions();
+  }
+ 
+  @Output() pageChanged = new EventEmitter<number>();
+ 
+  numberOfPages = 0;
+  pageOptions = [] as number[];
   currentPage = 1;
-
-
-  constructor() { 
+ 
+  constructor() {}
+ 
+  ngOnInit(): void {}
+ 
+  onPageClick(page: number) {
+    this.currentPage = page;
+    this.setPageOptions();
+    this.pageChanged.emit(this.currentPage);
+  }
+ 
+  setPageOptions() {
     this.pageOptions = [
-      this.currentPage -2 ,
+      this.currentPage - 2,
       this.currentPage - 1,
       this.currentPage,
       this.currentPage + 1,
-      this.currentPage + 2
-    ].filter(pageNumber => pageNumber>=1 && pageNumber <= this.numberOfPages);
+      this.currentPage + 2,
+    ].filter(
+      (pageNumber) => pageNumber >= 1 && pageNumber <= this.numberOfPages
+    );
   }
-
-  ngOnInit(): void {
-  }
-
 }
